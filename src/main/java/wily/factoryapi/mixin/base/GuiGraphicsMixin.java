0a12561050f03c7055f90ad0b3eb28e51a6b60e4
Jab125 <67534807+Jab125@users.noreply.github.com>
2026-06-06 16:20:34 +1000

    Update GuiGraphicsMixin.java

--- src/main/java/wily/factoryapi/mixin/base/GuiGraphicsMixin.java
+++ src/main/java/wily/factoryapi/mixin/base/GuiGraphicsMixin.java
@@ -22,7 +22,9 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.navigation.ScreenRectangle;
-import net.minecraft.client.renderer.MultiBufferSource;
+//? if <26.2 {
+/*import net.minecraft.client.renderer.MultiBufferSource;
+*///?}
 //? if <1.21.6 {
 /*import net.minecraft.client.renderer.RenderType;
 *///?}
@@ -83,8 +85,10 @@ public abstract class GuiGraphicsMixin implements FactoryGuiGraphics.Accessor {
     @Shadow public abstract Matrix3x2fStack pose();
     //?}
 
-    @Unique
+    //?if <26.2 {
+    /*@Unique
     private MultiBufferSource.BufferSource lastBufferSource;
+    *///?}
 
     //? if <=1.20.1
     //@Shadow abstract void innerBlit(ResourceLocation resourceLocation, int i, int j, int k, int l, int m, float f, float g, float h, float n);
@@ -326,28 +330,31 @@ public abstract class GuiGraphicsMixin implements FactoryGuiGraphics.Accessor {
             }
         }
         *///?}
-        @Override
+
+        //? if <26.2 {
+        /*@Override
         public MultiBufferSource.BufferSource getBufferSource() {
             //? if >=1.21.6 {
             return null;
             //?} else {
-            /*return bufferSource;
-            *///?}
+            /^return bufferSource;
+            ^///?}
         }
         @Override
         public void pushBufferSource(MultiBufferSource.BufferSource newBufferSource) {
             //? if <1.21.6 {
-            /*lastBufferSource = bufferSource;
+            /^lastBufferSource = bufferSource;
             bufferSource = newBufferSource;
-            *///?}
+            ^///?}
         }
 
         @Override
         public void popBufferSource() {
             //? if <1.21.6 {
-            /*if (lastBufferSource != null) bufferSource = lastBufferSource;
-            *///?}
+            /^if (lastBufferSource != null) bufferSource = lastBufferSource;
+            ^///?}
         }
+        *///?}
 
         @Override
         public FactoryGuiMatrixStack pose() {
